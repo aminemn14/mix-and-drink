b474ca23180f9e95043b2c65be729da48e0bc128
Amine Moumen <aamine.moumen@outlook.fr>
2025-05-03 18:58:34 +0200

    test: set up Vitest environment with happy-dom and global mocks

---
+++ vitest.setup.js
@@ -0,0 +1,23 @@
+import { config } from "@vue/test-utils";
+import { vi } from "vitest";
+
+config.global.mocks = {};
+
+global.fetch = vi.fn();
+
+const localStorageMock = (() => {
+  let store = {};
+  return {
+    getItem: vi.fn((key) => store[key] || null),
+    setItem: vi.fn((key, value) => {
+      store[key] = value.toString();
+    }),
+    clear: vi.fn(() => {
+      store = {};
+    }),
+  };
+})();
+
+Object.defineProperty(window, "localStorage", {
+  value: localStorageMock,
+});
